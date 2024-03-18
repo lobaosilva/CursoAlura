6ab1283a378824ad94025394278381f4a26e53b4
Lobão Silva <lobaocode@outlook.com>
2024-03-18 14:23:59 -0300

    Logica III - Modulo 1

---
+++ APartirDoZeroInicianteEmProgramação/desafio.js
@@ -0,0 +1,33 @@
+// 1. Mostre um alerta com a mensagem "Bem-vindo ao nosso site!".
+alert('Boas vindas ao nosso site!');
+
+// 2. Declare uma variável chamada nome e atribua a ela o valor "Lua".
+let nome = 'Lua';
+
+// 3. Crie uma variável chamada idade e atribua a ela o valor 25.
+let idade = 25;
+
+// 4. Crie uma variável chamada idade e atribua a ela o valor 25.
+let numeroDeVendas = 50;
+
+// 5. Defina uma variável saldoDisponivel e atribua a ela o valor 1000.
+let saldoDisponivel = 1000;
+
+// 6. Exiba um alerta com o texto "Erro! Preencha todos os campos."
+alert('Erro! Preencha todos os campos');
+
+// 7. Declare uma variável chamada mensagemDeErro e atribua a ela o valor "Erro! Preencha todos os campos." Agora exiba um alerta com o valor da variável mensagemDeErro.
+let mensagemDeErro = 'Erro! Preencha todos os campos';
+alert(mensagemDeErro);
+
+// 8. Use um prompt para perguntar o nome do usuário e armazená-lo na variável nome.
+nome = prompt('Qual o nome do usuário');
+
+// 9. Peça ao usuário para digitar sua idade usando um prompt e armazene-a na variável idade.
+idade = prompt('Qual a sua idade');
+
+// 10. Peça ao usuário para digitar sua idade usando um prompt e armazene-a na variável idade. Agora, a idade seja maior ou igual que 18, exiba um alerta com a mensagem "Pode tirar a habilitação!".
+idade = prompt('Qual a sua idade');
+if (idade >= 18) {
+    alert('Pode tirar a habilitação!');
+}
