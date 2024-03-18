918ecaf473e4fa5d5ae83d748ff2e0ec07d238e2
Lobão Silva <lobaocode@outlook.com>
2024-03-18 15:08:48 -0300

    Lógica III - Modulo 2

---
+++ APartirDoZeroInicianteEmProgramação/desafio2.js
@@ -0,0 +1,31 @@
+// 1. Pergunte ao usuário qual é o dia da semana. Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!". Caso contrário, mostre "Boa semana!".
+let diaDaSemana = prompt('Qual é o dia da semana?');
+if (diaDaSemana == 'Sábado' || diaDaSemana == 'Domingo') {
+    alert('Bom fim de semana!');
+} else {
+    alert('Boa semana!');
+}
+
+// 2. Verifique se um número digitado pelo usuário é positivo ou negativo. Mostre um alerta informando.
+let numero = prompt('Informe um número');
+if (numero >= 0) {
+    alert('Número positivo');
+} else {
+    alert('Número negativo');
+}
+
+// 3. Crie um sistema de pontuação para um jogo. Se a pontuação for maior ou igual a 100, mostre "Parabéns, você venceu!". Caso contrário, mostre "Tente novamente para ganhar.".
+let pontuacao = 105;
+if (pontuacao >= 100) {
+    console.log('Parabéns, você venceu!');
+} else {
+    console.log('Tente novamente para ganhar.');
+}
+
+// 4. Crie uma mensagem que informa o usuário sobre o saldo da conta, usando uma template string para incluir o valor do saldo.
+let saldoConta = 500;
+alert(`Seu saldo é de R$ ${saldoConta}`);
+
+// 5. Peça ao usuário para inserir seu nome usando prompt. Em seguida, mostre um alerta de boas-vindas usando esse nome.
+let nome = prompt('Qual o seu nome?');
+alert(`Bem vindo ${nome}`);
--- APartirDoZeroInicianteEmProgramação/logica-js-projeto_inicial/app.js
+++ APartirDoZeroInicianteEmProgramação/logica-js-projeto_inicial/app.js
@@ -1,7 +1,15 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 5;
-let chute = prompt('Escolha um número entre 1 e 10');
 
+let numeroSecreto = 2;
+console.log(numeroSecreto);
+
+let chute = prompt('Escolha um número entre 1 e 10');
+console.log('Valor do chute:', chute);
+
+console.log('Resultado da comparação:', chute == numeroSecreto);
 if (chute == numeroSecreto) {
-    console.log('Isso aí! Você descobriu o número secreto (5)');
+    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`);
+} else {
+    console.log('Valor do número secreto:', numeroSecreto);
+    alert('Você errou :(');
 }
