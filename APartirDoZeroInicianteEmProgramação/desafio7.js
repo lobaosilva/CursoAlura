59f9826b4e481f1e4faa8520bff59935ceeae7d7
Lobão Silva <lobaocode@outlook.com>
2024-03-20 20:40:53 -0300

    Lógica IV - Modulo 4

---
+++ APartirDoZeroInicianteEmProgramação/desafio7.js
@@ -0,0 +1,21 @@
+// 1. Crie uma lista vazia, com o nome listaGenerica.
+let listaGenerica = [];
+
+// 2. Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+let listaDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+
+// 3. Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+listaDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+listaDeProgramacao.push('Java', 'Ruby', 'GoLang');
+
+// 4. Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+nomes = ['JavaScript', 'Python', 'Go'];
+console.log(nomes[0]);
+
+// 5. Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+nomes = ['JavaScript', 'Python', 'Go'];
+console.log(nomes[1]);
+
+// 6. Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+nomes = ['JavaScript', 'Python', 'Go'];
+console.log(nomes[2]);
