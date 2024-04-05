dc71a1cc38f2025002e3855bb4bd369b0bd75ac1
Lobão Silva <lobaocode@outlook.com>
2024-04-04 22:42:49 -0300

    Lógica V - Modulo 5

---
+++ APartirDoZeroInicianteEmProgramação/desafio10.js
@@ -0,0 +1,44 @@
+// Desafio 2: Manipulando Arrays
+// Declarando uma variável do tipo array
+let minhaLista = [];
+
+// Adicionando elementos com push
+minhaLista.push(1, 2, 3);
+console.log("Adicionando elementos:", minhaLista);
+
+// Criando uma nova variável
+let outrosNumeros = [4, 5, 6];
+
+// Concatenando arrays
+let novaLista = minhaLista.concat(outrosNumeros);
+console.log("Juntando Arrays:", novaLista);
+
+// Desafio 3: Removendo elementos com pop
+// Removendo o último elemento
+novaLista.pop();
+console.log("Desafio 4:", novaLista);
+
+// Desafio 4:Embaralhando elementos em um array Incremente com as informações a partir do Desafio 2
+
+function embaralharArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+// Embaralhando novaLista
+novaLista = embaralharArray(novaLista);
+console.log("Embaralhando a Lista:", novaLista);
+
+// Desafio 5: Removendo duplicatas
+
+// Função para remover duplicatas de um array
+function removerDuplicatas(array) {
+    return [...new Set(array)];
+}
+
+// Testando a função com novaLista
+let novaListaSemDuplicatas = removerDuplicatas(novaLista);
+console.log("Remover duplicatas:", novaListaSemDuplicatas);
