1d17f50cdbbe2314b91c5220948697bf69f5fab3
Evandeilson "Lobão" Silva <lobaocode@outlook.com>
2024-10-08 16:06:26 -0300

    Update app.js
    Módulo VI- Aula 6

--- APartirDoZeroInicianteEmProgramação/praticando-logica-projeto_inicial/amigo-secreto/js/app.js
+++ APartirDoZeroInicianteEmProgramação/praticando-logica-projeto_inicial/amigo-secreto/js/app.js
@@ -2,14 +2,24 @@ let amigos = [];
 
 function adicionar() {
     let amigo = document.getElementById('nome-amigo');
+    if (amigo.value == '') {
+        alert('Informe o nome do amigo!');
+        return;
+    }
+
+    if (amigos.includes(amigo.value)) {
+        alert('Nome já adicionado!');
+        return;
+    }
+
     let lista = document.getElementById('lista-amigos');
 
     amigos.push(amigo.value);
-    
+
     if (lista.textContent == '') {
         lista.textContent = amigo.value;
     } else {
-        lista.textContent += ', ' + amigo.value;
+        lista.textContent = lista.textContent + ', ' + amigo.value;
     }
 
     amigo.value = '';
@@ -19,14 +29,19 @@ function adicionar() {
 }
 
 function sortear() {
-    embaralha(amigos);
+    if (amigos.length < 4) {
+        alert('Adicione pelo menos 4 amigos!');
+        return;
+    }
+
+    embaralhar(amigos);
 
     let sorteio = document.getElementById('lista-sorteio');
     for (let i = 0; i < amigos.length; i++) {
         if (i == amigos.length - 1) {
-            sorteio.innerHTML += amigos[i] + '-->' + amigos[0] + '<br>';
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] +' --> ' +amigos[0] + '<br/>';
         } else {
-            sorteio.innerHTML += amigos[i] + '-->' + amigos[i + 1] + '<br>';
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] +' --> ' +amigos[i + 1] + '<br/>';
         }
     }
 }
@@ -37,11 +52,10 @@ function excluirAmigo(index) {
     atualizarSorteio();
 }
 
-function embaralha(lista) {
+function embaralhar(lista) {
     for (let indice = lista.length; indice; indice--) {
         const indiceAleatorio = Math.floor(Math.random() * indice);
-        // Atribuição via destructuring
-        [lista[indice -1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
+        [lista[indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
     }
 }
 
@@ -54,16 +68,16 @@ function atualizarLista() {
     let lista = document.getElementById('lista-amigos');
     lista.innerHTML = '';
 
-
     for (let i = 0; i < amigos.length; i++) {
         // Cria um elemento de parágrafo para cada amigo
         let paragrafo = document.createElement('p');
         paragrafo.textContent = amigos[i];
-
+       
         // Adiciona um evento de clique para excluir o amigo
         paragrafo.addEventListener('click', function() {
             excluirAmigo(i);
         });
+
 
         // Adiciona o parágrafo à lista
         lista.appendChild(paragrafo);
